04132b67774a9d87d0016c1229f646c5ac30c2bc
Matthew deHaas <mattdehaas28@gmail.com>
2025-07-18 22:02:07 -0400

    Create packing slips from data.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # mom-operations-automation
 Script for creating packing slips and invoices for Meals On the Move.
+
+
+## Running
+
+Clone the repo and create a virtual environment
+
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+
+Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+
+Create the folders that are not on version control
+
+```
+mkdir data
+mkdir instance
+```
+
+
+Run the script. Include --get in *args in stdout if you would like to read from the google sheet and populate the database, otherwise it will assume the database is already there:
+
+```
+python3 main.py
+```
+
--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import sys
 import requests
 import pandas as pd 
 import sqlite3
+from jinja2 import Template
+import html
+from datetime import datetime
+import os
+
+
+
+
+# Establish a database conneciton
+conn = sqlite3.connect("instance/prod.db")
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
 
 
 # Get data from the google sheet
@@ -15,11 +27,6 @@ if len(sys.argv) > 1 and sys.argv[1].lower() == "--get":
 
     with open("data/production.csv", "wb") as f:
         f.write(response.content)
-
-
-    # Esablish a database conneciton
-    conn = sqlite3.connect("instance/prod.db")
-    cur = conn.cursor()
 
     cur.execute("DROP TABLE IF EXISTS production")
 
@@ -43,32 +50,138 @@ if len(sys.argv) > 1 and sys.argv[1].lower() == "--get":
 
     # Convert .csv format to .sql
     df = pd.read_csv("data/production.csv")
-    df.to_sql("production", conn, if_exists="replace", index=False)
+    df.to_sql("production", conn, if_exists="replace")
 
 
 
-# Number of unique customers
-conn = sqlite3.connect("instance/prod.db")
-cur = conn.cursor()
+# TODO: Create Packing Slips 
+# - For a given customer, create a new packing slip for every unqiue entry on the 'Shipment date' column
+# - Packing slip columns - all from the respective customer's order in the prod sheet (starred entires aren't in every sheet):
+#   - Customer
+#   - Serving
+#   - Item
+#   - Meal
+#   - Order
+#   - Quantity
+#   - Client
+#   - *Hot
+#   - *Chilled
+#   - *Frozen
+#   - # of Bags/pans
+# - Either:
+#   - Create a dataframe that mirroring the packing slip table
+#   - Could also export directly to a packing slip PDF 
 
-customers = cur.execute('SELECT * FROM production').fetchall()
 
-print(customers[0])
+# Get all rows in the Customer column
+customers_raw = cur.execute('SELECT Customer FROM production').fetchall()
 
-# print(cur.execute("SELECT * FROM produciton").description)
+# Get unique customers
+customers = list(set([col[0] for col in customers_raw]))
+
+for customer in customers:
+
+    # Get a list of unique dates
+    order_dates = list()
+
+    orders = cur.execute('''
+    SELECT 
+    "Customer", "Serving Date", "Item", "Meal", "Day", "Order Amount", "Quantity", Client, "Quantity to Produce/Ship", "Shipment Date" 
+    FROM production 
+    WHERE Customer = ?''', (customer, )).fetchall()
+
+
+    for order in orders:
+        if str(order["Shipment Date"]) not in order_dates:
+            order_dates.append(order["Shipment Date"])
+
+    # Could fail if the single digit days are entered as one digit
+    order_dates = sorted(order_dates, key=lambda d: datetime.strptime(d, "%d/%b/%y"))
+
+
+    # Create as many packing slips as there are unique dates
+    for date in order_dates:
+        
+
+        # Jinja2 HTML template for packing slip
+        template = Template("""
+
+            <h1>Packing Slip</h1>
+
+            <p>
+                <strong>Client:</strong> {{ client }}
+            </p>
+
+            <p>
+                <strong>Shipment Date:</strong> {{ date }}
+            </p>
+
+            <table border="1" cellpadding="5">
+
+                <tr>
+                    <th>Serving Date</th>
+
+                    <th>Item</th>
+
+                    <th>Meal</th>
+
+                    <th>Day</th>
+
+                    <th>Order Amount</th>
+
+                    <th>Client</th>
+
+                    <th>Quantity Shipped</th>
+                </tr>
+                    {% for order in orders %}
+                        <tr>    
+                            <td>{{ order['Serving Date'] or '' }}</td>
+
+                            <td>{{ order['Item'] or '' }}</td>
+
+                            <td>{{ order['Meal'] or '' }}
+
+                            <td>{{ order['Day'] or '' }}
+
+                            <td>{{ order['Order Amount'] or '' }}
+                            
+                            <td>{{ order['Client'] or '' }}
+
+                            <td>{{ order['Quantity to Produce/Ship'] or '' }}
+                            </td>
+                        </tr>
+                    {% endfor %}
+            </table>
+
+        """)
+
+        # Convert template to html
+        html = template.render(
+            client=customer,
+            date=date,
+            orders=orders
+        )
+
+            
+        os.makedirs(f"data/{customer}", exist_ok=True)
+        os.makedirs(f"data/{customer}/packing_slips", exist_ok=True)
+
+        with open(f"data/{customer}/packing_slips/{customer}_{str(date.replace('/', '_'))}.html", "w") as f:
+            f.write(html)
 
 
 
 
 
 
-
-# Create Packing Slips
-
+# TODO: Create Invoices
 
 
-# Create Invoices
 
+
+
+
+# TODO: Create shopping list
 
 
 
